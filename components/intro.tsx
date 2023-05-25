3f9ebb4b131010f5ae312015e30cd6ee96bd0550
João <pix.mardoica@gmail.com>
2023-05-25 19:28:36 -0300

    fix: intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,20 +4,22 @@ import { CMS_NAME } from "../lib/constants";
 const Intro = () => {
 	return (
 		<header className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-			<Image
-				src="/assets/images/Marx.png"
-				alt="Mardoica"
-				width={100}
-				height={100}
-				style={{
-					filter: "brightness(1.1)",
-					borderRadius: "50%",
-					marginRight: ".8rem",
-				}}
-			/>
-			<h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-				Mardoica
-			</h1>
+			<div className="flex flex-col md:flex-row items-center md:mr-20">
+				<Image
+					src="/assets/images/Marx.png"
+					alt="Mardoica"
+					width={100}
+					height={100}
+					style={{
+						filter: "brightness(1.1)",
+						borderRadius: "50%",
+						marginRight: ".8rem",
+					}}
+				/>
+				<h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+					Mardoica
+				</h1>
+			</div>
 			<h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
 				Meu nome é João, este blog, Mardoica, é o meu espaço de aprendizado em
 				público.
